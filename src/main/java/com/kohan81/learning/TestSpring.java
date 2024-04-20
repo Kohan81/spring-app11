2f839a95d51f55e6437d654e8ac4c09cc9d48c4f
kohan81 <kohan81@gmail.com>
2024-04-20 07:33:22 +0100

    Lesson 5
    Signed-off-by: kohan81 <kohan81@gmail.com>

--- src/main/java/com/kohan81/learning/TestSpring.java
+++ src/main/java/com/kohan81/learning/TestSpring.java
@@ -12,10 +12,10 @@ public class TestSpring {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        Music music = context.getBean("musicBean", Music.class);
+        //Music music = context.getBean("musicBean", Music.class);
+        //MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        MusicPlayer musicPlayer = new MusicPlayer(music);
-
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.play();
 
         context.close();
